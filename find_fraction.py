5badbaa289858ba414e294895f0d7c53a9c34b2e
username <username@example.com>
2022-08-12 16:04:02 +0300

    equal_equation final

--- find_fraction.py
+++
@@ -1,9 +0,0 @@
-from numpy import around
-
-
-def find_fraction(fraction: float) -> int:
-    for i in range(1, 50):
-        for j in range(1, 50):
-            if around(i/j, 2) == fraction:
-                return j
-    return 1
